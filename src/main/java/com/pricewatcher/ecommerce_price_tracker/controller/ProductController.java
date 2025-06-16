9c98742defc9935a6981ed015cea30916375c8b8
neutronfive <neutronfive@neutronfives-Mac-mini.local>
2025-06-16 19:28:36 +0530

    Save changes before pulling

--- src/main/java/com/pricewatcher/ecommerce_price_tracker/controller/ProductController.java
+++ src/main/java/com/pricewatcher/ecommerce_price_tracker/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")  // Handles requests to /products
+@RequestMapping("/products")  // Handles requests to fetch /products
 public class ProductController {
 
     private final ProductService productService;
